2e83539e9186523fe02a3c471c1e4f2b29e56585
Max <2870192@mail.ru>
2025-05-21 18:33:07 +0300

    Deploy to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 import CreatePoll from './pages/CreatePoll';
 import HomePage from './pages/HomePage';
@@ -10,13 +10,16 @@ import LoginPage from './pages/LoginPage';
 import PollPage from './pages/PollPage';
 
 const App: React.FC = () => {
+  const basename = process.env.NODE_ENV === 'production' ? '/voting-platform' : '/';
+
+
   return (
-    <Router>
+    <Router basename={basename}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePoll />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/vote/:id" element={<PollPage />} /> {/* маршрут голосования по id */}
+        <Route path="/vote/:id" element={<PollPage />} />
       </Routes>
     </Router>
   );
