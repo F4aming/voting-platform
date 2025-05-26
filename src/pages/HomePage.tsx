3519cd1c519b3f9870d7785998f4ee338c9f7a9e
Max <2870192@mail.ru>
2025-05-27 02:23:59 +0300

    homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -19,7 +19,9 @@ const HomePage: React.FC = () => {
               className="me-2"
               style={{ filter: 'grayscale(100%) brightness(0)' }}
             />
-            <span className="fw-bold">Голосовалка</span>
+            <a className="nav-link fw-bold" href="/" onClick={(e) => { e.preventDefault(); navigate('/'); }}>
+                Голосовалка
+            </a>
           </a>
 
           {/* Кнопка для мобильных устройств, чтобы раскрыть меню */}
